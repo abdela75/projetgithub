822a1263547d375ec29757edb7feb8c573c72f16
rsibi2019 <rsibi2019@localhost>
2020-11-12 14:54:47 +0100

    Test test

---
+++ projet JAVA/src/projetgithub.java
@@ -0,0 +1,15 @@
+
+public class projetgithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			int i = 0;
+			
+				for(i=0; i <= 10; i++) {
+					System.out.println("5x" + i + "=" + 5 * i);
+				}
+			
+	}
+
+}
